e6580c4ef79349ac289fbef1663d9a0a096fe2b3
Danni <d.youziel@gmail.com>
2019-01-29 23:02:34 +0000

    Abstracting callbacks

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const yargs = require('yargs');
 const geocode = require('./geocode/geocode');
 
-console.log(geocode)
-
 //setup details for commandline
 const argv = yargs
     .options({
@@ -18,4 +16,10 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-    geocode.geocodeAddress(argv.address);
+    geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+        if (errorMessage) {
+            console.log(errorMessage);
+        }else {
+            console.log(JSON.stringify(results, undefined, 2));
+        }
+    });
--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,9 +1,9 @@
 const request = require('request');
 
-function geocodeAddress(address) {
+function geocodeAddress(address, callback) {
         //store address as external variable
         let encodedAddress = encodeURIComponent(address);
-
+        //store key as external variable
         let geoKey = 'zGfoFArnJz7qqtzwBCE7LuAxD0yON8QI';
 
         request({
@@ -12,26 +12,28 @@ function geocodeAddress(address) {
         }, (error, response, body) => {
             if (error) {
                 //handle serve error
-                console.log('Cannot connect to MapQuest Server')
+                callback('Cannot connect to MapQuest Server')
             } else if (body.info.statuscode === 400){
-                console.log('No data retrieved');
+                callback('No data retrieved');
             } else if (((body.results[0].locations[0].geocodeQualityCode.substring(2)).match(/X/g) || []).length > 0){
                 //can alter above condition to change quality of user input  
                 console.log('Invalid Address, needs city, country or postcode');
                 } else if (body.info.statuscode === 0) {
-                    //provid search result against user input so they can verify result
-                    let locationInfo = body.results[0]
-                    console.log(`Provided Address: ${locationInfo.providedLocation.location}`)
-                    console.log(`Located Address: ${locationInfo.locations[0].street}, 
-                        ${locationInfo.locations[0].adminArea5}, 
-                        ${locationInfo.locations[0].adminArea4},
-                        ${locationInfo.locations[0].adminArea1},
-                        ${locationInfo.locations[0].postalCode}`);
-                    console.log(`Longitude: ${locationInfo.locations[0].latLng.lng}`)
-                    console.log(`Latitude: ${locationInfo.locations[0].latLng.lat}`)
+                    const locationInfo = body.results[0]
+                    callback(undefined, {
+                     providedAddress: locationInfo.providedLocation.location,
+                     locatedAddress:  locationInfo.locations[0].street +','+ 
+                                      locationInfo.locations[0].adminArea5 +','+  
+                                      locationInfo.locations[0].adminArea4 +','+  
+                                      locationInfo.locations[0].adminArea1 +','+  
+                                      locationInfo.locations[0].postalCode, 
+                    Longitude: locationInfo.locations[0].latLng.lng,
+                    Latitude: locationInfo.locations[0].latLng.lat
+                });
             }
-        });
+        })
     };
+
 
 
 
