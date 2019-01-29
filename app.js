c0d25dc69b99ffe3cc1a434bdf62a33751eab16e
Danni <d.youziel@gmail.com>
2019-01-29 15:34:17 +0000

    Handling errors
    Todo: Set quality level for user entry and handle all below quality entries.

--- app.js
+++ app.js
@@ -17,16 +17,26 @@ const argv = yargs
     .argv;
 
 //store address as external variable
-
 let encodedAddress = encodeURIComponent(argv.address);
 
 request({
-    url: `http://www.mapquestapi.com/geocoding/v1/address?key=OI1O0UlBKDjkR2e7claLT6xRAh1BZdVG&location=${encodedAddress}`,
+    url: `http://www.mapquestapi.com/geocoding/v1/address?key=zGfoFArnJz7qqtzwBCE7LuAxD0yON8QI&location=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
-    //prettify what is printed to console
-    //console.log(JSON.stringify(body, undefined, 2));
-    console.log(`Address: ${body.results[0].providedLocation.location}`);
+    if (error) {
+        //handle serve error
+        console.log('Cannot connect to Google Server')
+    } else if (body.results[0].locations[0].geocodeQualityCode.charAt(3) === 'X') {
+        //handle user error
+        console.log('Invalid Address')
+    } else if (body.info.statuscode === 0) {
+    console.log(`Provided Address: ${body.results[0].providedLocation.location}`)
+    console.log(`Located Address: ${body.results[0].locations[0].street}, 
+        ${body.results[0].locations[0].adminArea5}, 
+        ${body.results[0].locations[0].adminArea4},
+        ${body.results[0].locations[0].adminArea1},
+        ${body.results[0].locations[0].postalCode}`);
     console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`)
     console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`)
+    }
 })
