87f6572c66a8c666b06b0036103ec72b88c4b8a5
Danni <d.youziel@gmail.com>
2019-01-29 17:46:40 +0000

    Handled acceptions
    The condition can be altered in lines with MapQuest geoQualityCodes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const request = require('request');
 const yargs = require('yargs');
+const _ = require('lodash') 
 
 //setup details for commandline
 const argv = yargs
@@ -19,28 +20,31 @@ const argv = yargs
 //store address as external variable
 let encodedAddress = encodeURIComponent(argv.address);
 
+//Quality codes for MapQuest
+const mapQuestQualityCodes = ['P1', 'L1', 'I1', 'B1', 'B2', 'B3', 'A4', 'A5', 'A6', 'Z1', 'Z1', 'Z3', 'Z4'];
+
 request({
     url: `http://www.mapquestapi.com/geocoding/v1/address?key=zGfoFArnJz7qqtzwBCE7LuAxD0yON8QI&location=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
     if (error) {
         //handle serve error
-        console.log('Cannot connect to Google Server')
-        //ok quality for street number
-        //must have good quality street name
-        //must have good quality town/city
-        //ok if no postcode
-    } else if (body.results[0].locations[0].geocodeQualityCode.charAt(3) === 'X') {
-        //handle user error
-        console.log('Invalid Address')
-    } else if (body.info.statuscode === 0) {
-    console.log(`Provided Address: ${body.results[0].providedLocation.location}`)
-    console.log(`Located Address: ${body.results[0].locations[0].street}, 
-        ${body.results[0].locations[0].adminArea5}, 
-        ${body.results[0].locations[0].adminArea4},
-        ${body.results[0].locations[0].adminArea1},
-        ${body.results[0].locations[0].postalCode}`);
-    console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`)
-    console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`)
+        console.log('Cannot connect to MapQuest Server')
+    } else if (body.info.statuscode === 400){
+        console.log('No data retrieved');
+      } else if (((body.results[0].locations[0].geocodeQualityCode.substring(2)).match(/X/g) || []).length > 0){
+        //can alter above condition to change quality of user input  
+        console.log('Invalid Address, needs city, country or postcode');
+        } else if (body.info.statuscode === 0) {
+            //provid search result against user input so they can verify result
+            let locationInfo = body.results[0]
+            console.log(`Provided Address: ${locationInfo.providedLocation.location}`)
+            console.log(`Located Address: ${locationInfo.locations[0].street}, 
+                ${locationInfo.locations[0].adminArea5}, 
+                ${locationInfo.locations[0].adminArea4},
+                ${locationInfo.locations[0].adminArea1},
+                ${locationInfo.locations[0].postalCode}`);
+            console.log(`Longitude: ${locationInfo.locations[0].latLng.lng}`)
+            console.log(`Latitude: ${locationInfo.locations[0].latLng.lat}`)
     }
 })
