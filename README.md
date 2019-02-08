5f7f638964fc1e7860481ae4d49c38532643f9a2
Danni <d.youziel@gmail.com>
2019-02-08 10:27:43 +0000

    Refactored

--- README.md
+++ README.md
@@ -4,9 +4,17 @@ Geolocation weather app.
 The weather found is based on latitude and longitude coordinates. 
 
 USE:
-Download and in the terminal type
 
-node app.js -a "[ADDRESS]"
+To get the weather for a default address:
+node app.js 
+
+To get the weather for a specific address:
+node app.js -a '[ADDRESS]'
+
+To change default address:
+node app.js -d '[ADDRESS]'
+(This does not give results, only changes default)
+
 
 The address should have as much detail as possible to be accurate. 
 A street number and name will provide the weather, but it may not be 
@@ -14,7 +22,6 @@ the correct location, e.g. same street name and city but different city.
 A street number will result in an error.
 
 Todo
-- Make units yargs option
-- Make default address yargs option
-- Add song of the days based on weather
+- Make units and language yargs option
+- Add song of the day based on weather
 - Add date and time to forecast
--- app-promise.js
+++ app-promise.js
@@ -1,11 +1,9 @@
 const yargs = require('yargs');
-const axios = require('axios');
 const fs = require('fs');
+const getResults = require('./weather/weather-promise');
 
 
-console.log('________Starting WeatherApp__________')
-
-let encodedAddress;
+console.log('________Starting WeatherApp__________');
 
 const argv = yargs
     .options({
@@ -34,69 +32,31 @@ const argv = yargs
     .argv;
 
 if (argv.address) {
-     console.log('Use entered address');
-     encodedAddress = encodeURIComponent(argv.address);
+     let encodedAddress = encodeURIComponent(argv.address);
+
+    getResults.getWeatherII(encodedAddress);
+
  } else if (!argv.default) {
-     console.log('Use default address');
+     console.log('Using default address');
      try {
-         const address = JSON.parse(fs.readFileSync('defaultAddress.json'));
-         encodedAddress = encodeURIComponent(address);
+        let address = JSON.parse(fs.readFileSync('defaultAddress.json'));
+        let encodedAddress = encodeURIComponent(address);
+
+        getResults.getWeatherII(encodedAddress);
+        console.log(getResults.getWeatherII(encodedAddress));
+
      } catch (error) {
-         throw new Error('Default Address: File not found. Please enter address');
+         console.log(error)
+         throw new Error('No address found.');
      }
  }
 
-
-function defaultAddress() {
-     if (argv.default) {
-            console.log('Add/change default address');
-            fs.writeFileSync('defaultAddress.json', JSON.stringify(argv.default));
-     } 
-}
+if (argv.default) {
+    console.log('Changing default address');
+    fs.writeFileSync('defaultAddress.json', JSON.stringify(argv.default));
+} 
 
 
-console.log('address', argv.a);
-console.log('default', argv.d);
-console.log('ea', encodedAddress);
-console.log('da', defaultAddress());
 
 
-//store key as external variable
-let geoKey = 'zGfoFArnJz7qqtzwBCE7LuAxD0yON8QI';
-let geocodeURL = `http://www.mapquestapi.com/geocoding/v1/address?key=${geoKey}&location=${encodedAddress}`;
-
-axios.get(geocodeURL).then((response) => {
-    if (((response.data.results[0].locations[0].geocodeQualityCode.substring(2)).match(/X/g) || []).length > 1) {
-        throw new Error('Unable to find that address')
-    }
-    const locationInfo = response.data.results[0];
-    const lat = locationInfo.locations[0].latLng.lat;
-    const lng = locationInfo.locations[0].latLng.lng;
-
-    console.log('Here\'s the weather for: ', locationInfo.locations[0].street, locationInfo.locations[0].adminArea5,
-                            locationInfo.locations[0].adminArea4, locationInfo.locations[0].adminArea1, 
-                            locationInfo.locations[0].postalCode);
-    
-    const weatherKey = 'abf747543355ad6d3add9ac2eb0700f4';
-    const optionUnits = 'units=si';
-    
-    const weatherURL = `https://api.darksky.net/forecast/${weatherKey}/${lat},${lng}?${optionUnits}`;
-
-    return axios.get(weatherURL);
-
-}).then((response) => {
-
-    const temp = response.data.currently.temperature;
-    const apparentTemp = response.data.currently.apparentTemperature;
-    const summary = response.data.currently.summary;
-
-    console.log(`It's currently ${temp}C and ${summary}. It feels like ${apparentTemp}C`);
-
-}).catch(error => {
-    if (error.code === 'ENOTFOUND') {
-        console.log('Could not connect to MapRequest server');
-    } else {
-        console.log(error.message);
-    }
-});
 
